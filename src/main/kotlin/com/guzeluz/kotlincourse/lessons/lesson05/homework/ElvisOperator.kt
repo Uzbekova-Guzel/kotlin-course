11d0913603474cb085fed175fd98b9dd9099bb5e
guzeluzbekova <guzeluz.qa@gmail.com>
2025-09-16 22:33:34 +0200

    Added lesson05 homework 2

---
+++ src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson05/homework/ElvisOperator.kt
@@ -0,0 +1,42 @@
+package com.guzeluz.kotlincourse.lessons.lesson05.homework
+
+fun main() {
+    /*
+    Задача 1
+    Контекст: Вы изучаете физическое явление затухания звука в помещении.
+    У вас есть измеренное значение начальной интенсивности звука,
+    но из-за ограничений оборудования данные о коэффициенте затухания иногда могут быть неизвестны.
+    Задача: Рассчитать предполагаемую интенсивность звука после затухания.
+    Интенсивность звука после затухания пропорциональна начальной интенсивности, умноженной на коэффициент затухания.
+    Если коэффициент затухания неизвестен, использовать стандартное значение 0.5.
+    */
+
+    val startSoundIntensity = 50.0
+    val attenuationCoefficient: Double? = 0.1
+    val endSoundIntensity = startSoundIntensity * (attenuationCoefficient ?: 0.5)
+    println(endSoundIntensity)
+
+    /*
+    Задача 2
+    Контекст: Клиент оплачивает доставку груза.
+    К стоимости доставки добавляется страховка на груз, которая составляет 0,5% от его стоимости.
+    В случае, если стоимость не указана, то берётся стандартная стоимость в $50
+    Задача: Рассчитать полную стоимость доставки.
+    */
+
+    val deliveryCost: Double? = 45.0
+    val defaultDeliveryCost = 50.0
+    val cargoInsurance = (deliveryCost ?: defaultDeliveryCost) * 0.005
+    val finalDeliveryCost = (deliveryCost ?: defaultDeliveryCost) + cargoInsurance
+    println(finalDeliveryCost)
+
+    /*
+    Задача 3
+    Контекст: Вы проводите метеорологические измерения.
+    Одним из важных показателей является атмосферное давление, которое должно быть зафиксировано.
+    Лаборант приносит вам набор показателей, но по пути может что-нибудь потерять.
+    Задача - сообщить об ошибке в случае отсутствия показаний атмосферного давления.
+    */
+    val atmosphericPressure: String? = null
+    println(atmosphericPressure ?: "Atmospheric pressure indicators are empty!")
+}
