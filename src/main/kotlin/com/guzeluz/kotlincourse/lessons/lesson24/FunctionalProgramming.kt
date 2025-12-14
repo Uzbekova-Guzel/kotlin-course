469703b439473fb76aad5d358a9f452f7b0f1367
guzeluzbekova <guzeluz.qa@gmail.com>
2025-12-14 21:29:41 +0100

    Updated lesson24

--- src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson24/FunctionalProgramming.kt
+++ src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson24/FunctionalProgramming.kt
@@ -1,72 +1,74 @@
 package com.guzeluz.kotlincourse.lessons.lesson24
 
+import kotlin.random.Random
 
-//Функции высшего порядка — это функции, которые могут принимать
-// другие функции в качестве аргументов или возвращать их в качестве результата.
 
-// Источник ресурса E (топливо, электроэнергия, сжатый воздух,
-// корм для тягового животного)
-fun interface EnergySource<E> {
-    fun provide(): E
-}
-
-// Узел, который берёт ресурс E и превращает его в "двигательную мощность" P
-// (например: топливо -> крутящий момент вала,
-// электроэнергия -> крутящий момент, пар -> поступательное усилие,
-// корм -> движение мускулатуры)
-fun interface PowerUnit<E, P> {
-    fun generate(input: E, quantity: Double): P
-}
-
-// Узел, который берёт мощность P и превращает её в "движение" M
-// (крутящий момент -> скорость вращения колеса, тяга -> скорость,
-// тяга -> подъёмная сила и т.п.)
-fun interface MotionSystem<P, M> {
-    fun move(input: P): M
-}
-
-// Абстрактное транспортное средство:
-// E — тип исходного ресурса (топливо, энергия и т.п.)
-// P — тип мощности или усилия, которое создаёт силовая установка
-// M — тип "движения" на выходе (скорость, тяга, подъёмная сила, что угодно)
-abstract class Vehicle<E, P, M>(
-    private val energySource: EnergySource<E>,
-    private val powerUnit: PowerUnit<E, P>,
-    private val motionSystem: MotionSystem<P, M>
-) {
-    fun performMove(powerQuantity: Double): M {
-        val energy: E = energySource.provide()
-        val power: P = powerUnit.generate(energy, powerQuantity)
-        return motionSystem.move(power)
-    }
-}
-
-//примитивный пример реализации интерфейсов:
-class StringEnergy : EnergySource<String> {
-    override fun provide() = "Топливо"
-}
-
-class DoublePower : PowerUnit<String, Double> {
-    override fun generate(input: String, sourceQuantity: Double): Double {
-        return input.map { it.code / 2.0 }.sum() * sourceQuantity
-    }
-}
-
-class IntMotion : MotionSystem<Double, Int> {
-    override fun move(input: Double): Int {
-        return input.toInt() / 2
-    }
-}
-
-class StringIntMobil : Vehicle<String, Double, Int>(
-    StringEnergy(),
-    DoublePower(),
-    IntMotion()
-)
+////Функции высшего порядка — это функции, которые могут принимать
+//// другие функции в качестве аргументов или возвращать их в качестве результата.
+//
+//// Источник ресурса E (топливо, электроэнергия, сжатый воздух,
+//// корм для тягового животного)
+//fun interface EnergySource<E> {
+//    fun provide(): E
+//}
+//
+//// Узел, который берёт ресурс E и превращает его в "двигательную мощность" P
+//// (например: топливо -> крутящий момент вала,
+//// электроэнергия -> крутящий момент, пар -> поступательное усилие,
+//// корм -> движение мускулатуры)
+//fun interface PowerUnit<E, P> {
+//    fun generate(input: E, quantity: Double): P
+//}
+//
+//// Узел, который берёт мощность P и превращает её в "движение" M
+//// (крутящий момент -> скорость вращения колеса, тяга -> скорость,
+//// тяга -> подъёмная сила и т.п.)
+//fun interface MotionSystem<P, M> {
+//    fun move(input: P): M
+//}
+//
+//// Абстрактное транспортное средство:
+//// E — тип исходного ресурса (топливо, энергия и т.п.)
+//// P — тип мощности или усилия, которое создаёт силовая установка
+//// M — тип "движения" на выходе (скорость, тяга, подъёмная сила, что угодно)
+//abstract class Vehicle<E, P, M>(
+//    private val energySource: EnergySource<E>,
+//    private val powerUnit: PowerUnit<E, P>,
+//    private val motionSystem: MotionSystem<P, M>
+//) {
+//    fun performMove(powerQuantity: Double): M {
+//        val energy: E = energySource.provide()
+//        val power: P = powerUnit.generate(energy, powerQuantity)
+//        return motionSystem.move(power)
+//    }
+//}
+//
+////примитивный пример реализации интерфейсов:
+//class StringEnergy : EnergySource<String> {
+//    override fun provide() = "Топливо"
+//}
+//
+//class DoublePower : PowerUnit<String, Double> {
+//    override fun generate(input: String, sourceQuantity: Double): Double {
+//        return input.map { it.code / 2.0 }.sum() * sourceQuantity
+//    }
+//}
+//
+//class IntMotion : MotionSystem<Double, Int> {
+//    override fun move(input: Double): Int {
+//        return input.toInt() / 2
+//    }
+//}
+//
+//class StringIntMobil : Vehicle<String, Double, Int>(
+//    StringEnergy(),
+//    DoublePower(),
+//    IntMotion()
+//)
 
 //вариант с тем же автомобилем, но теперь вместо контрактов (интерфейсов)
 // сразу укажем сигнатуру необходимых функций через лямбда тип:
-abstract class LambdaVehicle<E, P, M> (
+abstract class LambdaVehicle<E, P, M>(
     private val energySource: () -> E,
     private val powerUnit: (E, Double) -> P,
     private val motionSystem: (P) -> M
@@ -78,16 +80,66 @@ abstract class LambdaVehicle<E, P, M> (
     }
 }
 
-class LambdaInterfaceStringIntMobil : LambdaVehicle<String, Double, Int>(
-    StringEnergy()::provide,
-    DoublePower()::generate,
-    IntMotion()::move
+//class LambdaInterfaceStringIntMobil : LambdaVehicle<String, Double, Int>(
+//    StringEnergy()::provide,
+//    DoublePower()::generate,
+//    IntMotion()::move
+//    /*Что такое ::provide? Это способ передачи функции в виде ссылки.
+//    То-есть, тут не происходит вызов функции, она в этот момент не выполняется.
+//    Тут мы сообщаем компилятору, что в качестве аргумента нужно использовать
+//    вот это название функции. И так как её сигнатура соответствует сигнатуре
+//    функции интерфейса, то она будет принята без ошибок.*/
+//)
+
+//способ написания кода функций непосредственно в виде аргументов класса.
+class LambdaStringIntMobile : LambdaVehicle<String, Double, Int>(
+    { "Топливо" },
+    { energy, sourceQuantity -> energy.map { it.code / 2.0 }.sum() * sourceQuantity },
+    { it.toInt() / 2 }
 )
 
+//Параметризация поведения
 
+//пример с параметризацией топлива для нашего класса
+class LambdaStringIntMobileParm(energySource: () -> String) : LambdaVehicle<String, Double, Int>(
+    energySource,
+    { energy, sourceQuantity -> energy.map { it.code / 2.0 }.sum() * sourceQuantity },
+    { it.toInt() / 2 }
+)
 
-fun main() {
-    val a = StringIntMobil()
-    println(a)
+//Передача функции в качестве аргумента
+fun String.charTransform(transformation: (Char) -> Char): String {
+//    this.map { transformation(it) }
+    return map(transformation).joinToString("")
 }
 
+fun main() {
+//    val m = StringIntMobil()
+//    println(m.performMove(2.0))
+//
+//    val a = LambdaStringIntMobile()
+//    println(a.performMove(2.0))
+
+    val r = {
+        Random.nextFloat().toString()
+    }
+    val parameterizedAuto = LambdaStringIntMobileParm(r)
+    println(parameterizedAuto)
+
+    val result = "my text".charTransform { it.uppercaseChar() }
+    println(result)
+    val result2 = "my text".charTransform { it + 1 }
+    println(result2)
+
+    listOf("").filter { it == "f" }
+
+    println(ex1("reswg")(3))
+}
+
+fun ex1(arg: String): (Int) -> String {
+    val a: (Int) -> String = {
+        (1..it).joinToString(" : ") { arg }
+    }
+    return a
+//    return { (1 .. it).map { arg }.joinToString(" : ") { arg } }
+}
