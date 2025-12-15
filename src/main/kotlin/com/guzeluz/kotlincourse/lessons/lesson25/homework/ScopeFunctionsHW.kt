d434835dd9363feeee3e0461e8eab554305c5145
guzeluzbekova <guzeluz.qa@gmail.com>
2025-12-15 11:56:44 +0100

    Updated lesson25 homework

--- src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson25/homework/ScopeFunctionsHW.kt
+++ src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson25/homework/ScopeFunctionsHW.kt
@@ -1,22 +1,24 @@
 package com.guzeluz.kotlincourse.lessons.lesson25.homework
 
-//ex1
-fun ex1(fnc: () -> Any): Long {
+/*Задание 1. Функция высшего порядка.
+Создайте функцию timeTracker, которая принимает другую функцию в качестве
+аргумента и измеряет время её выполнения.
+Функция timeTracker должна возвращать затраченное время в миллисекундах.
+Для измерения времени используйте System.currentTimeMillis() до и
+после выполнения переданной функции.*/
+fun timeTracker(fn: () -> Any): Long {
     val start = System.currentTimeMillis()
-    fnc()
+    fn()
     return System.currentTimeMillis() - start
 }
 
-//ex3
-fun printPerson(person: Person) {
-    with(person) {
-        println("""
-           Name: $name
-           Age: $age
-           Email: $email
-       """.trimIndent())
-    }
-}
+/*Задания для работы со scope функциями
+Давайте создадим два класса, которые будут использоваться в заданиях для
+демонстрации работы scope функций с преобразованием объектов и инициализацией
+или обновлением полей. Классы будут представлять собой Person и Employee,
+где Employee может быть создан на основе данных класса Person.
+
+Классы для выполнения заданий:*/
 
 class Person(val name: String, val age: Int) {
     var email: String = ""
@@ -27,36 +29,82 @@ class Employee(val name: String, val age: Int, val position: String) {
     var department: String = "General"
 }
 
+/*Задание 3: Использование also для логирования.
+Создайте функцию, принимающую объект Person и выводящую информацию об объекте
+в консоль в красивом, отформатированном виде.
+Используй with для создания контекста Person и
+возможности использовать поля объекта напрямую без обращения к переменной.
+Создайте объект Person и используйте also для добавления почты и
+печати данных в консоль с помощью метода который только что создали.*/
+fun personInfo(person: Person) {
+    with(person) {
+        println(
+            """
+            Name: $name
+            Age: $age
+            Email: $email
+            """.trimIndent()
+        )
+    }
+}
+
+/*Задание 6: Использование let для безопасного преобразования Person в Employee.
+Создайте функцию toEmployee, которая расширяет класс Person,
+который может быть null.
+В функции используйте функцию let для создания Employee,
+только если объект Person не null. В противном случае возвращается null*/
+fun Person?.toEmployee(position: String, depart: String): Employee? {
+    return this?.let {
+        Employee(it.name, it.age, position).apply {
+            email = it.email
+            department = depart
+        }
+    }
+}
 
 fun main() {
-//    ex1
+    //1
     val myFunction = {
         val list = List(10000000) { (0..10000).random() }
         list.sorted()
     }
-    println(ex1(myFunction))
+    println(timeTracker(myFunction))
 
-//    ex2
-    val em = Employee("Ivan", 23, "QA Engineer").apply {
-        email = "ivan@gm.com"
-        department = "dep1"
+    /*Задание 2: Использование apply для инициализации объекта Employee.
+    Создайте объект Employee и используйте apply для инициализации его
+    полей email и department.*/
+    val employee = Employee("Mike", 30, "Senior QA Engineer").apply {
+        email = "mike.QA@gmail.com"
+        department = "Marketplace Development"
     }
 
-//    ex3
-    val person = Person("Anna", 26).also {
-        it.email = "anna@gm.com"
-        printPerson(it)
+    //3
+    val person = Person("Mark", 28).also {
+        it.email = "mark.QA@gmail.com"
+        personInfo(it)
     }
 
-//    ex4
-    val person1 = Person("Andrew", 32).apply {
-        email = "andrew@gm.com"
+    /*Задание 4: Использование with для преобразования Person в Employee.
+    Создайте объект класса Person.
+    Используйте функцию with, чтобы создать из этого объекта Employee,
+    указав дополнительно должность.
+    Возвращаемым значением должен быть новый объект Employee.*/
+    val person1 = Person("Eva", 35).apply {
+        email = "eva.QA@gmail.com"
     }
-
-    val employee = with(person1) {
-        Employee(name, age, "Front dev").also {
+    val employee1 = with(person1) {
+        Employee(name, age, "QA Engineer").also {
             it.email = email
-            it.department = "dep2"
+            it.department = "Payment Department"
+        }
+    }
+
+    /*Задание 5: Использование run для преобразования и инициализации Employee.
+    Перепиши предыдущую задачу на использование run вместо with.*/
+    val employee11 = person1.run {
+        Employee(name, age, "QA Engineer").also {
+            it.email = email
+            it.department = "Payment Department"
         }
     }
 }
