ee04ec6a421132d22708af5493e0e9f59dbe781e
guzeluzbekova <guzeluz.qa@gmail.com>
2025-12-29 16:43:29 +0100

    Created lesson31 homework

---
+++ src/main/kotlin/com/guzeluz/kotlincourse/lessons/lesson31/homework/PhoneNumberFormatter.kt
@@ -0,0 +1,10 @@
+package com.guzeluz.kotlincourse.lessons.lesson31.homework
+
+class PhoneNumberFormatter {
+
+//    fun formatPhoneNumber(phone: String): String {
+//        try {
+//
+//        }
+//    }
+}
---
+++ src/test/kotlin/com/guzeluz/kotlincourse/lessons/lesson31/homework/PhoneNumberFormatterParameterizedTest.kt
@@ -0,0 +1,7 @@
+package com.guzeluz.kotlincourse.lessons.lesson31.homework
+
+import org.junit.jupiter.api.Assertions.*
+
+class PhoneNumberFormatterParameterizedTest {
+
+}
