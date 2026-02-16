8535a7b7b3268c7a758c6b54adcba21b1965276a
guzeluzbekova <guzeluz.qa@gmail.com>
2026-02-16 16:42:50 +0100

    Added kaspresso lesson05 homework

---
+++ src/main/kotlin/com/guzeluz/kotlincourse/lessons/kaspresso/lesson05/homework/Lesson05Kasp.kt
@@ -0,0 +1,108 @@
+package com.guzeluz.kotlincourse.lessons.kaspresso.lesson05.homework
+
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+/*
+Ниже представлена заготовка кода для создания расписания занятий.
+В один пункт расписания входит название урока, его время начала и завершения.
+Расписание состоит из этих пунктов, разбитых по дням недели без привязки к дате -
+каждую неделю одно и то же расписание.
+
+Твоя задача - дополнить класс Schedule методами, реализующими DSL,
+необходимый для удобного заполнения данными.
+Пример заполнения можно увидеть в закомментированном коде.
+
+Подсказки:
+1. Для создания диапазона через две точки переопредели оператор rangeTo
+2. Для передачи нужного дня недели используй внешнюю относительно метода переменную var,
+в которую нужно сохранить используемый в текущем блоке день недели и потом обнулить по завершении блока.
+ */
+
+data class ScheduleEntity(val lesson: String, val startTime: LocalTime, val endTime: LocalTime)
+
+enum class Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+class Schedule {
+
+    private val scheduleOfWeek = mutableMapOf<Days, MutableList<ScheduleEntity>>()
+    private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+
+    fun addSchedule(day: Days, scheduleEntity: ScheduleEntity) {
+        scheduleOfWeek.getOrPut(day) { mutableListOf() }.add(scheduleEntity)
+    }
+
+    override fun toString(): String {
+        return scheduleOfWeek.toSortedMap()
+            .map { (day, list) ->
+                list.sortedBy { it.startTime }
+                    .joinToString("\n") {
+                        "%-15s${it.startTime.format(timeFormatter)} - ${
+                            it.endTime.format(
+                                timeFormatter
+                            )
+                        }".format("\t${it.lesson}:")
+                    }.let {
+                        "${day.name.lowercase().replaceFirstChar { day.name[0].uppercase() }}:\n$it"
+                    }
+            }.joinToString("\n\n")
+    }
+}
+
+fun main() {
+
+    val schedule = Schedule()
+
+    // Так добавляется расписание без DSL
+    schedule.addSchedule(MONDAY, ScheduleEntity("Biology", LocalTime.of(10, 30), LocalTime.of(11, 10)))
+    schedule.addSchedule(MONDAY, ScheduleEntity("Chemistry", LocalTime.of(11, 15), LocalTime.of(11, 55)))
+
+    // Так добавляется расписание с использованием DSL
+//    schedule {
+//
+//        monday {
+//            "10:30".."11:10" schedule "Biology"
+//            "11:15".."11:55" schedule "Chemistry"
+//            "09:00".."09:40" schedule "Mathematics"
+//            "09:45".."10:25" schedule "History"
+//        }
+//
+//        tuesday {
+//            "09:00".."09:40" schedule "English"
+//            "09:45".."10:25" schedule "Geography"
+//            "11:15".."11:55" schedule "Art"
+//            "10:30".."11:10" schedule "Physics"
+//        }
+//
+//        wednesday {
+//            "11:15".."11:55" schedule "Biology"
+//            "09:00".."09:40" schedule "Literature"
+//            "10:30".."11:10" schedule "History"
+//            "09:45".."10:25" schedule "Mathematics"
+//        }
+//
+//        thursday {
+//            "11:15".."11:55" schedule "Physics"
+//            "10:30".."11:10" schedule "Geography"
+//            "09:00".."09:40" schedule "Chemistry"
+//            "09:45".."10:25" schedule "English"
+//        }
+//
+//        friday {
+//            "09:45".."10:25" schedule "Literature"
+//            "11:15".."11:55" schedule "History"
+//            "09:00".."09:40" schedule "Art"
+//            "10:30".."11:10" schedule "Mathematics"
+//        }
+//    }
+
+    println(schedule.toString())
+}
